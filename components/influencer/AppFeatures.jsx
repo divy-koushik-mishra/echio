e20de12c3692e03ef350c21d891e9202b6963d71
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-02-03 18:10:38 +0530

    code-cleanup

--- components/influencer/AppFeatures.jsx
+++ components/influencer/AppFeatures.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { arrowRight, brandImage } from "@/public/index";
+import { brandImage } from "@/public/index";
 
 export default function AppFeatures() {
 	return (

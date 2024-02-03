55e41c37f78e1a87de6ae7b2f666f68b510969a5
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-02-03 18:21:50 +0530

    agency-page-completed

--- pages/agency.jsx
+++ pages/agency.jsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Testimonials from "@/components/Testimonials";
 import AgencyAppFeatures from "@/components/agency/AgencyAppFeatures";
 import HeroAgencyPage from "@/components/agency/HeroAgencyPage";
 import React from "react";
@@ -9,6 +11,8 @@ export default function agency() {
 			<Navbar />
 			<HeroAgencyPage />
 			<AgencyAppFeatures />
+			<Testimonials />
+			<Footer />
 		</main>
 	);
 }
